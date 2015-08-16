bc17038ca5f72fd99edc87c18a3f4e3db66b3dba
SamJohnKing <samjohnkingchina@gmail.com>
2015-08-16 20:12:06 +0800

    2015-8-16-20-11 SamJohnking

--- YPS/src/yps_kernel_model/PortSimulator/yps_main.java
+++ YPS/src/yps_kernel_model/PortSimulator/yps_main.java
@@ -4,10 +4,7 @@ import org.lwjgl.LWJGLException;
 
 public class yps_main {
 	public static void main(String[] argv) throws Exception{
-		System.out.println(argv.length);
-		for(int i=0; i<argv.length; i++) System.out.println(argv[i]);
-		if((argv.length>1)&&(argv[1].equals("HelloWorld"))) System.out.println("=====>Hello World");
-		else SimulatorStart();
+		SimulatorStart();
 	}
 	public static void SimulatorStart() throws LWJGLException{
 		Game game = new port();
